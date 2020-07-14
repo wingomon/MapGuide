e661c13280fd1132a070d8c975e8aa0835dd63b5
wingex <m.diep@hotmail.de>
2020-07-14 23:18:07 +0200

    setup for firestore

--- app/src/main/java/com/example/mapguide/MainActivity.java
+++ app/src/main/java/com/example/mapguide/MainActivity.java
@@ -6,15 +6,23 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 //Wichtig: Das wird nicht die MainActivity sein!
 //ADD Guide Name
 
 public class MainActivity extends AppCompatActivity {
 
+    private StorageReference mStorageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Cloud Storage
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
 
