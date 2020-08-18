43356ae13ad115c01e4343632baa1a38f803554c
wingex <m.diep@hotmail.de>
2020-08-18 22:20:17 +0200

    Draw Routes (buggy and slow)
    We dont want to have optimized route, but simple route between marker

--- app/src/main/java/com/example/mapguide/StartCreateGuide_AddStationOverview.java
+++ app/src/main/java/com/example/mapguide/StartCreateGuide_AddStationOverview.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +24,11 @@ import android.widget.Toast;
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.api.optimization.v1.MapboxOptimization;
+import com.mapbox.api.optimization.v1.models.OptimizationResponse;
+import com.mapbox.geojson.LineString;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -37,14 +48,25 @@ import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import static com.mapbox.core.constants.Constants.PRECISION_6;
 import static com.mapbox.mapboxsdk.style.layers.Property.ICON_ROTATION_ALIGNMENT_VIEWPORT;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconSize;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
 
-public class StartCreateGuide_AddStationOverview extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener{
+public class StartCreateGuide_AddStationOverview extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener{
 
    private MapView mapView;
 
@@ -58,6 +80,17 @@ public class StartCreateGuide_AddStationOverview extends AppCompatActivity imple
     StationAdapter stationAdapter;
     Station tempStation;
     SymbolManager symbolManager;
+
+
+    private DirectionsRoute optimizedRoute;
+    private MapboxOptimization optimizedClient;
+    private Point origin;
+
+    private static final String ICON_GEOJSON_SOURCE_ID = "icon-source-id";
+    private static final String FIRST = "first";
+    private static final String ANY = "any";
+    private static final String TEAL_COLOR = "#FF0000";
+    private static final float POLYLINE_WIDTH = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,6 +126,182 @@ public class StartCreateGuide_AddStationOverview extends AppCompatActivity imple
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
 
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                // Add origin and destination to the mapboxMap
+                initMarkerIconSymbolLayer(style);
+                initOptimizedRouteLineLayer(style);
+                mapboxMap.addOnMapClickListener(StartCreateGuide_AddStationOverview.this);
+                enableLocationComponent(style);
+            }
+        });
+    }
+
+        private void initMarkerIconSymbolLayer(@NonNull Style loadedMapStyle) {
+            // Add the marker image to map
+            loadedMapStyle.addImage("icon-image", BitmapFactory.decodeResource(
+                    this.getResources(), R.drawable.marker));
+
+            // Add the source to the map
+            /**
+            loadedMapStyle.addSource(new GeoJsonSource(ICON_GEOJSON_SOURCE_ID,
+                    Feature.fromGeometry(Point.fromLngLat(origin.longitude(), origin.latitude()))));
+            **/
+
+            loadedMapStyle.addSource(new GeoJsonSource(ICON_GEOJSON_SOURCE_ID));
+
+            loadedMapStyle.addLayer(new SymbolLayer("icon-layer-id", ICON_GEOJSON_SOURCE_ID).withProperties(
+                    iconImage("icon-image"),
+                    iconSize(0.3f),
+                    iconAllowOverlap(true),
+                    iconIgnorePlacement(true),
+                    iconOffset(new Float[] {0f, -7f})
+            ));
+        }
+
+    private void initOptimizedRouteLineLayer(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addSource(new GeoJsonSource("optimized-route-source-id"));
+        loadedMapStyle.addLayerBelow(new LineLayer("optimized-route-layer-id", "optimized-route-source-id")
+                .withProperties(
+                        lineColor(Color.parseColor(TEAL_COLOR)),
+                        lineWidth(POLYLINE_WIDTH)
+                ), "icon-layer-id");
+    }
+
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+
+        clearMap();
+
+    // Optimization API is limited to 12 coordinate sets
+        if (alreadyTwelveMarkersOnMap()) {
+            Toast.makeText(this, "nur zwölf erlaubt", Toast.LENGTH_LONG).show();
+        } else {
+
+            //ADD NEW STATION
+            tempStation = new Station(stationList.size()+1, point.getLongitude(), point.getLatitude(),"Titel der Station","null","null","Beschreibung der Station");
+            stationList.add(tempStation);
+            stationAdapter.notifyDataSetChanged();
+
+            Style style = mapboxMap.getStyle();
+            if (style != null) {
+                addDestinationMarker(style, point);
+                //Ich brauche für getOptimizedRoute eine Liste mit Punkten von der Stationsliste
+                List<Point> stationPointList = new ArrayList<>();
+                for(Station s: stationList) {
+                    stationPointList.add(Point.fromLngLat(s.getLongitude(), s.getLatitude()));
+                }
+                if(stationList.size()>=2) {
+                    getOptimizedRoute(style, stationPointList);
+                }
+            }
+        }
+        return true;
+    }
+
+
+    private void clearMap() {
+
+        Log.d("Mapbox","Clear Map (should show when Map was clicked");
+        if (mapboxMap != null) {
+            Style style = mapboxMap.getStyle();
+            if (style != null) {
+                resetDestinationMarkers(style);
+                removeOptimizedRoute(style);
+            }
+        }
+    }
+
+    private void resetDestinationMarkers(@NonNull Style style) {
+        GeoJsonSource optimizedLineSource = style.getSourceAs(ICON_GEOJSON_SOURCE_ID);
+        /**
+        if (optimizedLineSource != null) {
+            optimizedLineSource.setGeoJson(Point.fromLngLat(origin.longitude(), origin.latitude()));
+        }**/
+    }
+
+    private void removeOptimizedRoute(@NonNull Style style) {
+        GeoJsonSource optimizedLineSource = style.getSourceAs("optimized-route-source-id");
+        /**
+        if (optimizedLineSource != null) {
+            optimizedLineSource.setGeoJson(FeatureCollection.fromFeatures(new Feature[] {}));
+        }**/
+    }
+
+    private boolean alreadyTwelveMarkersOnMap() {
+        return stationList.size() == 12;
+    }
+
+    private void addDestinationMarker(@NonNull Style style, LatLng point) {
+        List<Feature> destinationMarkerList = new ArrayList<>();
+        for (Station s : stationList) {
+            destinationMarkerList.add(Feature.fromGeometry(
+                    Point.fromLngLat(s.getLongitude(), s.getLatitude())));
+            Log.d("MAPBOX", "AddDestinationMarker: Iteration durch stationList, Hinzufügen aller Punkte");
+        }
+        //destinationMarkerList.add(Feature.fromGeometry(Point.fromLngLat(point.getLongitude(), point.getLatitude())));
+        GeoJsonSource iconSource = style.getSourceAs(ICON_GEOJSON_SOURCE_ID);
+        if (iconSource != null) {
+            iconSource.setGeoJson(FeatureCollection.fromFeatures(destinationMarkerList));
+        }
+    }
+
+
+    private void getOptimizedRoute(@NonNull final Style style, List<Point> coordinates) {
+        optimizedClient = MapboxOptimization.builder()
+                .source(FIRST)
+                .destination(ANY)
+                .coordinates(coordinates)
+                .overview(DirectionsCriteria.OVERVIEW_FULL)
+                .profile(DirectionsCriteria.PROFILE_WALKING)
+                .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : getString(R.string.mapbox_access_token))
+                .build();
+
+        optimizedClient.enqueueCall(new Callback<OptimizationResponse>() {
+            @Override
+            public void onResponse(Call<OptimizationResponse> call, Response<OptimizationResponse> response) {
+                if (!response.isSuccessful()) {
+                    Timber.d("no success");
+
+                } else {
+                    if (response.body() != null) {
+                        List<DirectionsRoute> routes = response.body().trips();
+                        if (routes != null) {
+                            if (routes.isEmpty()) {
+                                Timber.d("%s size = %s", "Successfull but no routes", routes.size());
+                            } else {
+                    // Get most optimized route from API response
+                                optimizedRoute = routes.get(0);
+                                drawOptimizedRoute(style, optimizedRoute);
+                            }
+                        } else {
+                            Timber.d("list of routes in the response is null");
+
+                        }
+                    } else {
+                        Timber.d("response.body() is null");
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<OptimizationResponse> call, Throwable throwable) {
+                Timber.d("Error: %s", throwable.getMessage());
+            }
+        });
+    }
+
+    private void drawOptimizedRoute(@NonNull Style style, DirectionsRoute route) {
+        GeoJsonSource optimizedLineSource = style.getSourceAs("optimized-route-source-id");
+        if (optimizedLineSource != null) {
+            optimizedLineSource.setGeoJson(FeatureCollection.fromFeature(Feature.fromGeometry(
+                    LineString.fromPolyline(route.geometry(), PRECISION_6))));
+        }
+    }
+/**
         StartCreateGuide_AddStationOverview.this.mapboxMap = mapboxMap;
 
         Log.d("--MAPREADY--","OnMapReady was loaded");
@@ -144,7 +353,7 @@ public class StartCreateGuide_AddStationOverview extends AppCompatActivity imple
                         });
                     }
                 });
-    } //End onMapReady()
+    } //End onMapReady()**/
 
     @SuppressWarnings( {"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
