fea2bdd19076f5bb9cd2b0b96adf527d7e0438df
wingex <m.diep@hotmail.de>
2020-09-09 00:08:34 +0200

    implemented Login/Logout

---
+++ app/src/main/java/com/example/mapguide/StarCreateGuideLoginControl.java
@@ -0,0 +1,14 @@
+package com.example.mapguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StarCreateGuideLoginControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_create_guide_login_control);
+    }
+}
---
+++ app/src/main/java/com/example/mapguide/UserPage.java
@@ -0,0 +1,13 @@
+package com.example.mapguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_page);
+    }
+}
